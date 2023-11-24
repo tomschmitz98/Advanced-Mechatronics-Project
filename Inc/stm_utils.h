9cd09333f9d510492b85ce98f61ccb9ba52bff9a
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-11-23 21:40:19 -0500

    Documented utils

--- Inc/stm_utils.h
+++ Inc/stm_utils.h
@@ -1,8 +1,10 @@
-/*
- * stm_utils.h
+/**
+ * @file stm_utils.h
+ * @version 1.0
+ * @brief   Defines bitfields, useful macros, and any useful, general purpose
+ *          types. This file is compatible with the STM32F446C and STM32F446E
  *
- *  Created on: Jun 8, 2023
- *      Author: Tom
+ * @author Tom Schmitz <tschmitz@andrew.cmu.edu>
  */
 
 #ifndef STM_UTILS_H_
@@ -10,6 +12,7 @@
 
 #include <stdint.h>
 
+/* Useful 16-bit Bitfield Macros */
 #define BIT0 ((uint32_t)0x0001)
 #define BIT1 ((uint32_t)0x0002)
 #define BIT2 ((uint32_t)0x0004)
@@ -27,21 +30,27 @@
 #define BITE ((uint32_t)0x4000)
 #define BITF ((uint32_t)0x8000)
 
+/* Useful macro for using 16-bit macros in the upper 16-bits */
 #define UPPER16BITS(BITFIELD) (((uint32_t)BITFIELD) << 16)
 
-#define SET_BIT(BITFIELD, N) (BITFIELD |= ((uint32_t)0x1 << N))
+/* Useful macros for setting, clearing, and checking a certain bit */
+#define SET_BIT(BITFIELD, N)   (BITFIELD |= ((uint32_t)0x1 << N))
 #define CLEAR_BIT(BITFIELD, N) (BITFIELD &= ~((uint32_t)0x1 << N))
 #define CHECK_BIT(BITFIELD, N) ((BITFIELD >> N) & 0x1)
 
-typedef enum {
-	bank_a = 0x000,
-	bank_b = 0x100,
-	bank_c = 0x200,
-	bank_d = 0x300,
-	bank_e = 0x400,
-	bank_f = 0x500,
-	bank_g = 0x600,
-	bank_h = 0x700
+/**
+ * @brief   Defines the different GPIO banks
+ */
+typedef enum
+{
+    bank_a = 0x000,
+    bank_b = 0x100,
+    bank_c = 0x200,
+    bank_d = 0x300,
+    bank_e = 0x400,
+    bank_f = 0x500,
+    bank_g = 0x600,
+    bank_h = 0x700
 } gpio_bank_t;
 
 #endif /* STM_UTILS_H_ */
