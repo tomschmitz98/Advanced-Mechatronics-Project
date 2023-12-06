b5d2b4dd7d9337e1cede7025ce1bf72853141b4b
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-12-06 12:20:25 -0500

    Added button states

---
+++ Core/Inc/button_states.h
@@ -0,0 +1,13 @@
+/*
+ * button_states.h
+ *
+ *  Created on: Dec 6, 2023
+ *      Author: Tom
+ */
+
+#ifndef INC_BUTTON_STATES_H_
+#define INC_BUTTON_STATES_H_
+
+
+
+#endif /* INC_BUTTON_STATES_H_ */
---
+++ Core/Src/button_states.c
@@ -0,0 +1,8 @@
+/*
+ * button_states.c
+ *
+ *  Created on: Dec 6, 2023
+ *      Author: Tom
+ */
+
+
