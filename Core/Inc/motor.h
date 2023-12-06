ffb67717f5dee36a7437d2238fde152b1260965d
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-12-06 12:18:05 -0500

    Added motor files

---
+++ Core/Inc/motor.h
@@ -0,0 +1,13 @@
+/*
+ * motor.h
+ *
+ *  Created on: Dec 6, 2023
+ *      Author: Tom
+ */
+
+#ifndef INC_MOTOR_H_
+#define INC_MOTOR_H_
+
+
+
+#endif /* INC_MOTOR_H_ */
---
+++ Core/Src/motor.c
@@ -0,0 +1,8 @@
+/*
+ * motor.c
+ *
+ *  Created on: Dec 6, 2023
+ *      Author: Tom
+ */
+
+#include "motor.h"
