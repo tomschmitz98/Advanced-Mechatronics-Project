fcaf65854c275b12829661a524b830d807d6476f
Tom Schmitz <thomas.schmitz98@outlook.com>
2023-12-06 12:11:47 -0500

    Added button io

---
+++ Core/Inc/button_io.h
@@ -0,0 +1,21 @@
+/*
+ * button_io.h
+ *
+ *  Created on: Dec 6, 2023
+ *      Author: Tom
+ */
+
+#ifndef INC_BUTTON_IO_H_
+#define INC_BUTTON_IO_H_
+
+#include <stdbool.h>
+
+typedef enum {
+	PAUSE_BUTTON = 0,
+	START_BUTTON = 1
+} button_t;
+
+void init_buttons(void);
+bool read_button(button_t btn);
+
+#endif /* INC_BUTTON_IO_H_ */
